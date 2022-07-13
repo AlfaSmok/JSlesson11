6446242d06cb9a4691b45cfe041eea372983e92c
Alfir Zagidullin <alfir1991@gmail.com>
2022-07-13 22:41:15 +0300

    выполнено 11 задание

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+'use strick';
+
+const btn = document.getElementById('btn');
+const inputColor = document.getElementById('text');
+const square = document.getElementById('square');
+const eBtn = document.getElementById('e_btn');
+const inputRange = document.getElementById('range');
+const circle = document.getElementById('circle');
+const rangeSpan = document.getElementById('range-span');
+
+const colorName = function () {
+  square.style.backgroundColor = inputColor.value;
+};
+
+const chaneRange = function () {
+  circle.style.height = inputRange.value + '%';
+  circle.style.width = inputRange.value + '%';
+  rangeSpan.textContent = inputRange.value;
+};
+
+
+btn.addEventListener('click', colorName);
+eBtn.style.display = 'none';
+inputRange.addEventListener('input', chaneRange);
 
 
 
-
-
+console.log(btn);
+console.log(inputColor);
+console.dir(circle);
